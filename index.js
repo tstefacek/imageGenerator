34f4cc9db12e6ef20ae5f50c470140cf9c3adfe6
Tashe Stefacek <tstefacek92@gmail.com>
2023-08-10 23:59:25 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const path = require('path');
+const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,9 @@ const app = express();
 //Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 app.listen(port, () => console.log(`Server started on port ${port}`));
